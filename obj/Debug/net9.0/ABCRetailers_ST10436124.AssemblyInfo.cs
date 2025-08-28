3dcdd3fcbba8b20bf0632d50eab379c4e6796131
BooleanBile <oarabile.marwane1@gmail.com>
2025-08-28 18:24:20 +0200

    last push

--- obj/Debug/net9.0/ABCRetailers_ST10436124.AssemblyInfo.cs
+++ obj/Debug/net9.0/ABCRetailers_ST10436124.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers_ST10436124")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509ed30fa31d6cfb4e00b8a0293190bf0a286468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495a9455f0b0bc00f2fadab714c613b67ef9a8f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers_ST10436124")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers_ST10436124")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
